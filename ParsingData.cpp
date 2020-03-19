24428d64c71c9a684511cc06f8cfb6c043cc97e6
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-03-19 22:56:12 +0200

    Reading for separator /F

--- ParsingData.cpp
+++ ParsingData.cpp
@@ -7,15 +7,14 @@ using namespace std;
 
 void parsing(){//Назар не читай єту хрень пока я не допишу))
 string word, def;
-word = "";
-def = "";
-char buf ;
 ifstream inn;
 inn.open("dictionary.txt");
 for(int i = 0; i < /*количество слов в словаре*/ ; i++){
 getline(inn, word, ';');
 getline(inn, def, '\n');
 hashTable.addEl(word, def);
+word = "";
+def = "";
 }
 inn.close();
 }
