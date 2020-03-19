eeb0b09d25cd4c7ba29f7f55f8e5e0c76afca53d
SoVa-web <54186792+SoVa-web@users.noreply.github.com>
2020-03-19 22:53:36 +0200

    Reading for separator

--- ParsingData.cpp
+++ ParsingData.cpp
@@ -1,10 +1,21 @@
 #include<iostream>
 #include<string>
 #include<fstream>
+#include "HashTable.h"
 
-void parsing(){
-string word, meaning;
+using namespace std;
+
+void parsing(){//Назар не читай єту хрень пока я не допишу))
+string word, def;
+word = "";
+def = "";
+char buf ;
 ifstream inn;
 inn.open("dictionary.txt");
+for(int i = 0; i < /*количество слов в словаре*/ ; i++){
+getline(inn, word, ';');
+getline(inn, def, '\n');
+hashTable.addEl(word, def);
+}
 inn.close();
 }
